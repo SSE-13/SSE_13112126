44c6e91cee89674ecc44bc68398968455e805071
Shensijia <857745486@qq.com>
2016-04-07 13:05:19 +0800

    3

--- mouseevent/game.ts
+++ mouseevent/game.ts
@@ -1,23 +1,52 @@
-
 var humanContainer = new render.DisplayObjectContainer();
 var head = new render.Bitmap();
-head.x = 100;
-head.source = "wander-icon.jpg";
-humanContainer.addChild(head);
+head.source = "head.png";
+head.x=50;
+head.y=100;
+humanContainer.addChild(head)
 
+var trunk = new render.Bitmap();
+trunk.source = "trunk.png";
+trunk.x=65;
+trunk.y=135;
+humanContainer.addChild(trunk)
+
+var R_arm = new render.Bitmap();
+R_arm.source = "R_arm.png";
+R_arm.x=40;
+R_arm.y=135;
+humanContainer.addChild(R_arm)
+
+var L_arm = new render.Bitmap();
+L_arm.source = "L_arm.png";
+L_arm.x=70;
+L_arm.y=135;
+humanContainer.addChild(L_arm)
+
+var R_leg = new render.Bitmap();
+R_leg.source = "R_leg.png";
+R_leg.x=35;
+R_leg.y=190;
+humanContainer.addChild(R_leg)
+
+var L_leg = new render.Bitmap();
+L_leg.source = "L_leg.png";
+L_leg.x=65;
+L_leg.y=190;
+humanContainer.addChild(L_leg)
 
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+renderCore.start(humanContainer, ["head.png","trunk.png","R_arm.png","L_arm.png","R_leg.png","L_leg.png"]);
+
+
 
 class HumanBody extends Body {
-    
-    
+   
     vx:number = 5;
-    
-
+   
     onTicker(duringTime: number) {
-        this.x = 100;//+= duringTime * this.vx;
-        this.y = 100;
+        this.x += this.vx*duringTime;//+= duringTime * this.vx;
+        this.rotation += 2*Math.PI*duringTime;
 
     }
 }
@@ -37,6 +66,17 @@ var headHitTest = (localPoint:math.Point,displayObject:render.DisplayObject) =>{
 
 var headOnClick = () => {
     alert("clicked!!");
+  //  body.vx=-5;
+    //修改 HumanBody 的速度，使其反向移动
+}
+var LegHitTest = (localPoint:math.Point,displayObject:render.DisplayObject) =>{
+    alert (`点击位置为${localPoint.x},${localPoint.y}`);
+    return true;
+}
+
+var LegOnClick = () => {
+    alert("clicked!!");
+  //  body.vx=-5;
     //修改 HumanBody 的速度，使其反向移动
 }
 
